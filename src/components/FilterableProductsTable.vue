0b749f6de96c2f1fe049c84fd7194b3e48749e8c
rgcunha <ruben.cunha@gmail.com>
2018-02-26 02:29:19 +0100

    Map actions and state to FilterableProductsTable

--- src/components/FilterableProductsTable.vue
+++ src/components/FilterableProductsTable.vue
@@ -2,40 +2,40 @@
   <div class='filterable-products-table'>
     <SearchBar @searchParamsUpdated='filterProducts($event)' />
     <ProductsTable :products='filteredProducts' />
-    <Loader v-if='isLoading'></Loader>
+    <Loader v-if='isFetching'></Loader>
   </div>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex';
 import SearchBar from './SearchBar';
 import ProductsTable from './ProductsTable';
 import Loader from './Loader';
-import FixedTermProduct from '../lib/api/resources/fixed-term-product';
+
+const DEFAULT_SEARCH_PARAMS = {
+  investmentAmount: '30000',
+  maturity: null,
+};
 
 export default {
   name: 'FilterableProductsTable',
   components: { SearchBar, ProductsTable, Loader },
   data() {
     return {
-      products: [],
       filteredProducts: [],
-      isLoading: false,
     };
   },
   created() {
-    const fixedTermProduct = new FixedTermProduct({ countryCode: 'de' });
-    this.isLoading = true;
-    fixedTermProduct.getAll()
-      .then(([...products]) => {
-        this.products = products;
-        this.filteredProducts = products;
-      })
-      .finally(() => {
-        this.isLoading = false;
+    this.fetchProducts()
+      .then(() => {
+        this.filterProducts(DEFAULT_SEARCH_PARAMS);
       });
   },
   methods: {
-    filterProducts({ investmentAmount, maturity }) {
+    ...mapActions([
+      'fetchProducts',
+    ]),
+    filterProducts({ investmentAmount, maturity = null }) {
       const isEligibleForMaturity = ((product) => {
         if (!maturity) { return true; }
         return product.term_in_months === +maturity;
@@ -50,6 +50,10 @@ export default {
         .filter(isEligibleForMaturity);
     },
   },
+  computed: mapState({
+    products: state => state.products,
+    isFetching: state => state.isFetching,
+  }),
 };
 </script>
 
